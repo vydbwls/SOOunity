61c05c795b9a3cff2a7972b6b3196da8ad93fe2b
Jeon Dayeon <wjsekdus1@naver.com>
2022-03-29 02:14:07 +0900

    0329MovingAnimation
    MovingAnimation Update with Daemeori

--- SurviverOfOtome/Assets/Unan/Scripts/MovingObjects.cs
+++ SurviverOfOtome/Assets/Unan/Scripts/MovingObjects.cs
@@ -16,52 +16,68 @@ public class MovingObjects : MonoBehaviour
 
     private bool CanMove = true;
 
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     IEnumerator MoveCoroutine()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        while(Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
         {
-            applyRunSpeed = runSpeed;
-            applyRun = true;
-        }
-
-        else
-        {
-            applyRunSpeed = 0;
-            applyRun = false;
-        }
-
-        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
-
-        while (currentWalkCount < walkCount)
-        {
-            if (vector.x != 0)
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+                applyRunSpeed = runSpeed;
+                applyRun = true;
             }
 
-            else if (vector.y != 0)
+            else
             {
-                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                applyRunSpeed = 0;
+                applyRun = false;
             }
 
-            if(applyRun)
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            if(vector.x != 0)
             {
+                vector.y = 0;
+            }
+
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+            animator.SetBool("Walking", true);
+
+            while (currentWalkCount < walkCount)
+            {
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+                }
+
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                }
+
+                if (applyRun)
+                {
+                    currentWalkCount++;
+                }
+
                 currentWalkCount++;
+                yield return new WaitForSeconds(0.01f);
             }
-
-            currentWalkCount++;
-            yield return new WaitForSeconds(0.01f);
+            currentWalkCount = 0;
         }
-        currentWalkCount = 0;
 
+        animator.SetBool("Walking", false);
         CanMove = true;
     }
+        
     // Update is called once per frame
     void Update()
     {
