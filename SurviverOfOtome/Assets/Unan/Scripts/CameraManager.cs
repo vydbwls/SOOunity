31523b0182735b1bc8b91d4e6454eca47fa9835a
Jeon Dayeon <wjsekdus1@naver.com>
2022-04-13 21:16:55 +0900

    04/13Cameramove
    Cameramanager and CameraMove

--- SurviverOfOtome/Assets/Unan/Scripts/CameraManager.cs
+++ SurviverOfOtome/Assets/Unan/Scripts/CameraManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    public GameObject target;
+    public float moveSpeed;
+    private Vector3 targetPosition;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(target.gameObject != null)
+        {
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
     }
 }
