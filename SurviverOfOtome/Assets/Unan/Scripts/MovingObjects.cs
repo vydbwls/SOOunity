e0ae9976c3c87544e28c3b9e68f0fe0cfaf663c4
JeonDayeon <wjsekdus1@gmail.com>
2022-04-13 13:16:19 +0900

    04/13 crash
    Boxcollision crash

--- SurviverOfOtome/Assets/Unan/Scripts/MovingObjects.cs
+++ SurviverOfOtome/Assets/Unan/Scripts/MovingObjects.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MovingObjects : MonoBehaviour
 {
+
+    private BoxCollider2D boxCollider;
+    public LayerMask layerMask;
+
     public float speed;
     private Vector3 vector;
 
@@ -14,13 +18,14 @@ public class MovingObjects : MonoBehaviour
     public int walkCount;
     private int currentWalkCount;
 
-    private bool CanMove = true;
+    public bool CanMove = true;
 
     public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
     }
 
@@ -49,6 +54,18 @@ public class MovingObjects : MonoBehaviour
 
             animator.SetFloat("DirX", vector.x);
             animator.SetFloat("DirY", vector.y);
+
+            RaycastHit2D hit;
+
+            Vector2 start = transform.position;
+            Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount);
+
+            boxCollider.enabled = false;
+            hit = Physics2D.Linecast(start, end, layerMask);
+            boxCollider.enabled = true;
+
+            if (hit.transform != null) break;
+
             animator.SetBool("Walking", true);
 
             while (currentWalkCount < walkCount)
