bc77ffcbe4a2f1fa318180b582dea28b9e26b738
JeonDayeon <95409013+JeonDayeon@users.noreply.github.com>
2022-08-17 14:31:06 +0900

    ChangeBGMManager
    BGM Manager 코드 변경

--- SurviverOfOtome/Assets/Unan/Scripts/BGMmanager.cs
+++ SurviverOfOtome/Assets/Unan/Scripts/BGMmanager.cs
@@ -2,30 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGMmanager : MonoBehaviour
+[System.Serializable]
+public class BGM
 {
+    public string name;
+    public AudioClip clip;
+};
+
+public class BGMManager : MonoBehaviour
+{
+    public static BGMManager instance;
+
+    [SerializeField] BGM[] BGM = null;
+
+
     private AudioSource audioSource;
 
     public AudioClip[] clips;
-    public int ibgm;
+    public string bgmName;
     // Start is called before the first frame update
     void Start()
     {
        audioSource = GetComponent<AudioSource>();
-       Play();
-       //audioSource.Play();
+        Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //audioSource.clip = clips[ibgm];
     }
 
     void Play()
     {
-        audioSource.clip = clips[ibgm];
-        audioSource.Play();
+        for(int i = 0; i < BGM.Length; i++)
+        {
+            if(bgmName == BGM[i].name)
+            {
+                audioSource.clip = clips[i];
+                audioSource.Play();
+            }
+        }
     }
 
     void Stop()
